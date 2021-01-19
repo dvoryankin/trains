852664a90ca4d341c49b55d7869a1bf64ba1a3c2
dvoryankin <dvoryankin@gmail.com>
2021-01-19 16:21:41 +0300

    add crud views for train

--- db/migrate/20210115132015_create_trains.rb
+++ db/migrate/20210115132015_create_trains.rb
@@ -1,6 +1,7 @@
 class CreateTrains < ActiveRecord::Migration[6.0]
   def change
     create_table :trains do |t|
+      t.integer :number
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_19_115319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "railway_stations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.integer "number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
