dc88a5f82e7b91aa53b53730a607ebe91134d93c
dvoryankin <dvoryankindm@gmail.com>
2021-03-04 14:20:42 +0300

    Carriage model move to carriages

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Trains
     config.load_defaults 6.0
     config.autoloader = :classic
 
+    config.autoload_paths += %W(#{config.root}/app/models/carriages)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
