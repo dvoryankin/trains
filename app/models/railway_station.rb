a0fccb67c140fcef83e91298f3f4378fbb259421
dvoryankin <dvoryankindm@gmail.com>
2021-03-14 21:09:56 +0300

    position Route station

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,4 +4,22 @@ class RailwayStation < ApplicationRecord
   has_many :trains, foreign_key: :current_station_id
 
   validates :title, presence: true
+
+  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
+
+  def update_position(route, position)
+    station = route_station(route)
+    station.update(position: position) unless station.nil?
+  end
+
+  def position_in(route)
+    station = route_station(route)
+    station.position unless station.nil?
+  end
+
+  private
+
+  def route_station(route)
+    railway_stations_routes.where(route_id: route).first
+  end
 end
---
+++ db/migrate/20210314180643_add_position_to_railway_station_routes.rb
@@ -0,0 +1,5 @@
+class AddPositionToRailwayStationRoutes < ActiveRecord::Migration[6.0]
+  def change
+    add_column :railway_stations_routes, :position, :integer
+  end
+end
